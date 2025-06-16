3fbde9c99d8bdf8ed556cfdb8fa5fb27aec8ea23
smallslowtank <small.slow.tank@gmail.com>
2025-06-16 21:08:31 +0400

    add query last_quote

--- queries/read.py
+++ queries/read.py
@@ -37,3 +37,36 @@ async def random_quote():
         quote = data.quote
         author = data.author
         return quote, author
+
+
+async def last_quote(user_id):
+    """
+    Получить идентификатор последней цитаты для пользователя
+    """
+
+    async def callee(pool: ydb.aio.QuerySessionPool, user_id):
+        result = await pool.execute_with_retries(
+            """
+            DECLARE $parameter1 AS Int64;
+
+            SELECT quote_id FROM Users
+            WHERE user_id = $parameter1;
+            """,
+            {
+                "$parameter1": user_id
+            },
+        )
+
+        return result
+
+    async with ydb.aio.Driver(
+            connection_string=os.getenv('YDB_CS'),
+            credentials=ydb.iam.MetadataUrlCredentials(),
+    ) as driver:
+        await driver.wait(timeout=5, fail_fast=True)
+
+        async with ydb.aio.QuerySessionPool(driver) as pool:
+            result = await callee(pool, user_id)
+
+        data = result[0].rows[0].quote_id
+        return data
