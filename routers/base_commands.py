332a9a35f5abcc1fc8c49baee3d89b53a44e07e7
smallslowtank <small.slow.tank@gmail.com>
2025-06-17 10:22:23 +0400

    edit base_commands.py, add check quote

--- routers/base_commands.py
+++ routers/base_commands.py
@@ -2,9 +2,9 @@ from aiogram import Router, types, F
 from aiogram.filters import CommandStart, or_f
 from aiogram.types import InputMediaPhoto, URLInputFile
 
+from crud import add_user, last_quote, random_quote, update_user
 from filters.chat_types import ChatTypeFilter
 from keyboards.inline import get_callback_buttons
-from queries.read import random_quote
 from core.config import settings
 
 router = Router(name=__name__)
@@ -34,10 +34,31 @@ async def start_menu(callback: types.CallbackQuery):
     """
     Коллбек "click"
     """
-    quote, author = await random_quote()
+
+    # Получить идентификатор пользователя телеграм
+    user = callback.from_user
+    user_id = user.id
+
+    # Получить последнюю цитату для пользователя
+    last_quote_id = await last_quote(user_id)
+    if 1 > last_quote_id or last_quote_id > 10:
+        # Если такого идентификатора нет в базе, получить цитату и добавить пользователя в базу
+        quote_id, quote, author = await random_quote()
+        await add_user(user_id, quote_id)
+    else:
+        # Иначе получить цитату и сравнить с последней
+        quote_id, quote, author = await random_quote()
+        while last_quote_id == quote_id:
+            # Если цитаты совпали, то получить цитату ещё раз
+            quote_id, quote, author = await random_quote()
+        # Обновить последнюю цитату пользователя в базе
+        await update_user(user_id, quote_id)
+
+    # Текст и изображение для ответа
     caption = f"{quote}\n🔖 {author}"
     photo = settings.URL_IMG
 
+    # Отправить (редактировать) сообщение
     await callback.message.edit_media(
         media=InputMediaPhoto(media=URLInputFile(url=photo), caption=caption),
         reply_markup=get_callback_buttons(
